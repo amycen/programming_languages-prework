48620b85c6e562e8f1d34ee55d0ccf90787cac6b
Amy Cen <amy.cen@blackrock.com>
2019-04-16 21:32:45 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,9 +3,17 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, langs_list|
     langs_list.each do |lang, type|
+<<<<<<< HEAD
       new_hash[lang]= {
           type: type[:type],
           style: []
+=======
+      new_hash = {
+        lang => {
+          type: type,
+          style: []
+        }
+>>>>>>> 0cd79af03117ae8cdf99154d3b0a5a326321c4c6
       }
     end
   end
@@ -16,3 +24,37 @@ def reformat_languages(languages)
   end
   new_hash
 end
+
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+}
+
+reformat_languages(languages)
